e9ed5a06454dcc6d00958ae6c53f19b43a80532a
DESKTOP-OUL4KTD\Ahmed Eletol <ahmed.eletol@gmail.com>
2017-08-31 19:08:22 +0200

    adding frontend and angular folder strucure

---
+++ BeersFactory.FrontEnd/Angular_App/Components/Landing/js/landingController.js
@@ -0,0 +1,7 @@
+"use strict";
+angular.module("App.landing").controller("landingController", function landingFunction($scope, $rootScope, $location, ngAuthSettings, authService) {
+    //$rootScope.UserName = authService.authentication.Name;
+    //$rootScope.isAuth = authService.authentication.isAuth;
+    //$rootScope.getNotificationCount();
+    //$rootScope.connectNotificationHub();
+});
---
+++ BeersFactory.FrontEnd/Angular_App/Shared/Services/lookUpService.js
@@ -0,0 +1,122 @@
+﻿'use strict';
+
+angular.module('App.shared').factory('lookUpService', ['$http', '$location', '$q', 'localStorageService', 'ngAuthSettings', 'appSetting', function ($http, $location, $q, localStorageService, ngAuthSettings, appSetting) {
+    var lookUpServiceFactory = {};
+    var baseUri = ngAuthSettings.baseUri;
+
+    var serviceBase = appSetting.AppSetting.serviceBase;
+    lookUpServiceFactory.getChannels = function () {
+        var url = serviceBase + 'api/Channel';
+
+        return $http({
+            method: 'GET',
+            url: url,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    };
+    lookUpServiceFactory.getPaymentMethods = function () {
+        var url = serviceBase + 'api/Payment/GetPaymentMethods';
+
+        return $http({
+            method: 'GET',
+            url: url,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    };
+    lookUpServiceFactory.getPaymentTypes = function () {
+        var url = serviceBase + 'api/Payment/GetPaymentTypes';
+
+        return $http({
+            method: 'GET',
+            url: url,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    };
+    lookUpServiceFactory.getPaymentStatuses = function () {
+        var url = serviceBase + 'api/Payment/GetPaymentStatuses';
+
+        return $http({
+            method: 'GET',
+            url: url,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    };
+    lookUpServiceFactory.getTicketStatuses = function () {
+        var url = serviceBase + 'api/TicketStatus';
+
+        return $http({
+            method: 'GET',
+            url: url,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    };
+    lookUpServiceFactory.getAgents = function () {
+        var url = serviceBase + 'api/Users/';
+
+        return $http({
+            method: 'GET',
+            url: url,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    };
+    lookUpServiceFactory.getRoles = function () {
+        var url = baseUri + 'api/Account/GetRoles';
+
+        return $http({
+            method: 'GET',
+            url: url,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    };
+    lookUpServiceFactory.getAgentsFiltered = function (filter) {
+        var url = serviceBase + 'api/Users/GetUserFiltered';
+
+        return $http({
+            method: 'POST',
+            url: url,
+            data:filter,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    };
+    lookUpServiceFactory.getAgents = function () {
+        var url = serviceBase + 'api/Users';
+
+        return $http({
+            method: 'GET',
+            url: url,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    };
+    lookUpServiceFactory.GetByTicketId = function (ticketId) {
+        var url = serviceBase + 'api/Customers/GetByTicketId?ticketId=' + ticketId;
+
+        return $http({
+            method: 'GET',
+            url: url,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    };
+
+    lookUpServiceFactory.getCustomerByPhoneNumber = function (phoneNumber) {
+        var url = serviceBase + 'api/Customers/GetCustomerByPhoneNumber?phoneNumber=' + phoneNumber;
+
+        return $http({
+            method: 'GET',
+            url: url,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    };
+    lookUpServiceFactory.getCustomerByPhoneOrEmail = function (phoneNumber, email) {
+        if (email == undefined) {
+            email = '';
+        }
+        var url = serviceBase + 'api/Customers/GetCustomerByPhoneOrEmail?phoneNumber=' + phoneNumber + '&email=' + email;
+
+        return $http({
+            method: 'GET',
+            url: url,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    };
+    return lookUpServiceFactory;
+}]);
