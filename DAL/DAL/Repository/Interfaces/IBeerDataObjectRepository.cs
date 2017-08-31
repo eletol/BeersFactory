d3cee85dd0968ff21c9650268680cbd4c0220f7d
DESKTOP-OUL4KTD\Ahmed Eletol <ahmed.eletol@gmail.com>
2017-08-31 19:08:42 +0200

    adding data access layer

---
+++ DAL/DAL/Repository/Interfaces/IBeerDataObjectRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using ZenOfBeer.BreweryDb.Pcl;
+
+namespace DAL.Repository.Interfaces
+{
+    public interface IBeerDataObjectRepository :IBaseRepository<BeerDataObject>
+    {
+         new  Task<BeerDataObject> GetByID(object id);
+    }
+}
